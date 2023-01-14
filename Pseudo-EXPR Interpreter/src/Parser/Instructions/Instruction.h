bb968b823af15b5d50d30b8cb2e086311a7c2c6b
PoinP <denkata142@gmail.com>
2023-01-14 18:11:23 +0200

    Minor change

--- Pseudo-EXPR Interpreter/src/Parser/Instructions/Instruction.h
+++ Pseudo-EXPR Interpreter/src/Parser/Instructions/Instruction.h
@@ -7,10 +7,19 @@
 class Instruction
 {
 public:
+	Instruction()
+	{}
 	virtual ~Instruction()
 	{}
 
 	virtual void run(Environment* env = nullptr) = 0;
+
+private:
+	Instruction(const Instruction& other) = delete;
+	Instruction(Instruction&& other) = delete;
+
+	Instruction& operator= (const Instruction& other) = delete;
+	Instruction& operator= (Instruction&& other) = delete;
 };
 
 #endif // !INSTRUCTION_H
